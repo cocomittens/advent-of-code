60087b136cd9e173df56f2457c20ac37d5dd0915
Corrie Stoddard <corriestoddard@gmail.com>
2023-12-01 13:39:08 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+import { calculateAnswer } from "./solutions/day1.js";
+
+console.log("Day 1:", calculateAnswer());
---
+++ solutions/day1.js
@@ -0,0 +1,9 @@
+import { data } from "../data/day1.js";
+
+const calculateCalibrations = () => {
+  data.filter((item) => {});
+};
+
+export const calculateAnswer = () => {
+  return 3;
+};
