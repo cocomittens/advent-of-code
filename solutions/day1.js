3ee9bb9cbe4e4e450dda6802b293dab4a5e9dcbe
Corrie Stoddard <corriestoddard@gmail.com>
2023-12-01 14:00:24 -0800

    Day 1 part 1 solution

--- solutions/day1.js
+++ solutions/day1.js
@@ -1,9 +1,29 @@
 import { data } from "../data/day1.js";
 
+function isNum(char) {
+  return /^\d$/.test(char);
+}
+
 const calculateCalibrations = () => {
-  data.filter((item) => {});
+  return data.map((item) => {
+    let first = null;
+    let last = null;
+    let i = 0;
+    let j = item.length - 1;
+    while (!first) {
+      if (isNum(item[i])) first = item[i];
+      i++;
+    }
+    while (!last) {
+      if (isNum(item[j])) last = item[j];
+      j--;
+    }
+    return parseInt(first + last);
+  });
 };
 
 export const calculateAnswer = () => {
-  return 3;
+  const calibrations = calculateCalibrations();
+  const sum = calibrations.reduce((a, c) => a + c);
+  return sum;
 };
