87c887026b388767632b66c1788f96bf64c9a2b1
Corrie Stoddard <corriestoddard@gmail.com>
2023-12-09 17:41:12 -0800

    Attempt day 6

--- solutions/day6.js
+++ solutions/day6.js
@@ -1,6 +1,29 @@
 import { data } from "../data/day6";
 
-const processRace = (race) => {};
+const isWin = (speed, time, distance) => {
+  return speed * time >= distance;
+};
+
+const processRace = (race) => {
+  let start = null;
+  let end = null;
+  let i = 1;
+  const time = race[0];
+  const distance = race[1];
+
+  while (!end && time <= i) {
+    const win = isWin(i, time - i, distance);
+    if (win && !start) {
+      start = i;
+    }
+    if (!win && start) {
+      end = i;
+    }
+    i++;
+  }
+
+  return end - start;
+};
 
 const processRaces = (races) => {
   const times = races.map((race) => processRace(race));
